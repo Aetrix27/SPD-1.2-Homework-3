b78d29f35c261eabb0131ef6fe5d1e21fd288031
Aetrix27 <davedogg323@gmail.com>
2020-11-11 10:41:06 -0800

    Added Carousel to Discover

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
-
 function addCart(){
-  $('exampleModal').modal.('hide');
+  $('exampleModal').modal('hide');
   showAlert("Item Added To Cart", "alert-success")
 
 }
 
 function showAlert(message, alertType){
 
-  $('exampleAlert').append($#'exampleAlert').append(
-    '<div id = "alertdiv" class = "alert alert-dismissable fixed-top" + alertType + >
-        '+message+'
-    </div>'
-  )
+  $('exampleAlert').append( $('exampleAlert')).append(
+    '<div id = "alertdiv" class = "alert alert-dismissable fixed-top' + alertType + '">' +
+    '<a class="close" data-dismiss="alert" aria-label="close" > *</a>' +
+        '<span>'+message+'</span>'+
+    '</div>' )
+  );
+
+  setTimeout(function(){
+    $('#alertdiv').remove();
+  }, 3000);
 }
